9041950912011ff743df51e74654c7298a1b7def
jordonbc <jordonbc@hotmail.co.uk>
2016-12-04 17:18:06 +0000

    V 1.0.0.0
    Created Menus

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1480871805,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/button-sheet0.png",
+		"images/menubackground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/cursor-sheet0.png",
+		"media/the_freeharmonic_orchestra_-_08_-_unconquered_sun.m4a",
+		"media/the_freeharmonic_orchestra_-_08_-_unconquered_sun.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"main menu background.mp4",
+		"loading video.mp4"
+	]
+}
