9318134a19dcd796638d07a29ee4c3a02aa9e9f6
jordonbc <jordonbc@hotmail.co.uk>
2016-12-20 23:01:36 +0000

    Added Credits

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1482235394,
+	"version": 1482274849,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -19,6 +19,11 @@
 		"images/mpcursor-sheet0.png",
 		"images/touchcenter-sheet0.png",
 		"images/touchmove-sheet0.png",
+		"images/alienwarscreditslogomini-sheet0.png",
+		"images/alienwarscreditslogosquare-sheet0.png",
+		"images/alienwarscreditslogowide-sheet0.png",
+		"images/alienwarscreditslogotext-sheet0.png",
+		"images/thefreeharmonicorchestralogo-sheet0.png",
 		"media/the_freeharmonic_orchestra_-_08_-_unconquered_sun.m4a",
 		"media/the_freeharmonic_orchestra_-_08_-_unconquered_sun.ogg",
 		"icon-16.png",
